8ffe887d854d67561ff891a84876cf18fbc2f1b6
nathanpao <91172301+nathanpao@users.noreply.github.com>
2022-08-13 02:28:45 -0700

    Add files via upload

---
+++ basicprograms/studentdetails.py
@@ -0,0 +1,5 @@
+studentid = int(input("Enter Student ID: "))
+name = input("Enter Student Name: ")
+marks = float(input("Enter Marks scored: "))
+
+print("ID:",studentid,"\nName:",name,"\nMarks:",marks)
