b89339fb1d5dfc52bb4412766fe7ab53a8ca7e1d
nathanpao <91172301+nathanpao@users.noreply.github.com>
2022-08-13 02:31:37 -0700

    Add files via upload

---
+++ functions/mathdemo.py
@@ -0,0 +1,10 @@
+from math import *
+import math
+
+print(sqrt(9))
+print(ceil(8.2))
+print(floor(7.9))
+print(fabs(-2.2))
+
+print(dir(math))
+help(math)
---
+++ functions/mymath.py
@@ -0,0 +1,11 @@
+'''
+Created on Jun 27, 2021
+
+@author: natha
+'''
+
+def sum(x,y):
+    return x+y
+
+def diff(x,y):
+    return x-y
